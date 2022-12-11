c9a92a622feb03e69df09c47a2391da1413138cd
MichaelZhLiu <95732957+MichaelZhLiu@users.noreply.github.com>
2022-12-10 21:31:51 -0800

    Add files via upload

---
+++ src/chassis.cpp
@@ -0,0 +1,20 @@
+#include "main.h"
+using namespace okapi::literals;
+
+
+  const int8_t leftFront = 1;
+  const int8_t leftBack = 2;
+  const int8_t rightFront = 3;
+  const int8_t rightBack = 4;
+
+
+
+
+/*
+void SetDriveMotors()
+{
+int leftJoystick = controller.get_analog(pros:: E_CONTROLLER_ANALOG_LEFT_Y);
+int rightJoystick = controller.get_analog(pros:: E_CONTROLLER_ANALOG_RIGHT_Y);
+SetDrive(leftJoystick, rightJoystick);
+}
+*/
---
+++ src/lift.cpp
@@ -0,0 +1,5 @@
+#include "headers/lift.hpp"
+
+const int8_t liftPort = 1;
+
+okapi::Motor lift = okapi::Motor(liftPort);
