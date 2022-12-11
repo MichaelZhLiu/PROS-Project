8a4af79a7b0550230211a176ff49e8c21316341b
MichaelZhLiu <95732957+MichaelZhLiu@users.noreply.github.com>
2022-12-10 21:38:57 -0800

    Add files via upload

---
+++ src/chassis.hpp
@@ -0,0 +1,11 @@
+#include  "main.h"
+
+void SetDrive(int left, int right);
+
+extern const int8_t leftFront;
+extern const int8_t leftBack;
+extern const int8_t rightFront;
+extern const int8_t rightBack;
+
+
+void SetDriveMotors();
